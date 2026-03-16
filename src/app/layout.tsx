768939efddc52a1d2b2b9d1677d630fc596ec8b1
Omar-Maher-Ahmed <om8523302@gmail.com>
2026-03-16 17:27:15 +0200

    [Add]  react-snowfall component to display a visual snowfall effect.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 'use client';
 import './globals.css';
+import ReactSnowfall from "react-snowfall";
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ReactSnowfall color="#22D3EE" snowflakeCount={100} speed={[0.5, 1]} wind={[0.5, 1]} />
+        {children}
+      </body>
     </html>
   );
 }
